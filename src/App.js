cbc0f8b34eb8b6b561b2937d4ce5fe0dca1f649d
alexismarroquin7 <alexismarroquin754@gmail.com>
2021-03-15 13:20:22 -0700

    create and connect clearDisplay action creator

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import TotalDisplay from './components/TotalDisplay';
 import CalcButton from './components/CalcButton';
 
 import reducer, { initialState } from "./reducers";
-import { addOne, applyNumber, changeOperation } from "./actions";
+import { addOne, applyNumber, changeOperation, clearDisplay } from "./actions";
 
 function App() {
 
@@ -62,7 +62,7 @@ function App() {
             </div>
 
             <div className="row ce_button">
-              <CalcButton value={"CE"}/>
+              <CalcButton value={"CE"} onClick={() => dispatch(clearDisplay())}/>
             </div>
 
           </form>
--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,8 @@ export const ADD_ONE = "ADD_ONE";
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
 
+export const CLEAR_DISPLAY = "CLEAR_DISPLAY";
+
 export const addOne = () => {
     return({type:ADD_ONE});
 }
@@ -13,4 +15,8 @@ export const applyNumber = (number) => {
 
 export const changeOperation = operationSign => {
     return({ type:CHANGE_OPERATION, payload:operationSign });
+};
+
+export const clearDisplay = () => {
+    return({ type:CLEAR_DISPLAY });
 };
